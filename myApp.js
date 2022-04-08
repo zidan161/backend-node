5532cfb7f99e6bb9f80d395fa773a85d4b4f8fd0
zidan161 <zidanam161@gmail.com>
2022-04-08 08:48:50 +0700

    express static

--- myApp.js
+++ myApp.js
@@ -3,6 +3,8 @@ var app = express();
 
 var path = __dirname + '/views/index.html'
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(req, res) {
     res.sendFile(path);
 })
